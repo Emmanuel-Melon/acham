588cae501b9f1f3c85347d5473c83ccb211c1b89
Eman <emmanuelgatwech@gmail.com>
2020-01-10 21:43:48 +0300

    added makeup pae

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Claudia Acham`,
+    title: `Acham`,
     description: `Claudia Acham is a Beatuty Expert & Makeup Artist from South Sudan. She offers bridal, editorial and day packages.`,
     author: `@gatsbyjs`,
   },
--- src/pages/makeup.js
+++ src/pages/makeup.js
@@ -1,4 +1,5 @@
 import React from "react"
+import styles from './makeup.module.css'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -6,7 +7,47 @@ import SEO from "../components/seo"
 const MakeupPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>Makeup</h1>
+    <div className={styles.wrapper}>
+      <div className={styles.header}>
+        <h1>Book an Incredible Makeup Experience</h1>
+        <p>
+          Hi I’m Anony! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula augue vitae nibh porta mollis. Morbi nec pretium justo. Vestibulum sed ante dignissim, eleifend urna nec, blandit eros.
+        </p>
+        <button className='action'>Get Started</button>
+      </div>
+      <div className={styles.plans}>
+        <div>
+          <h3>Bridal</h3>
+          <p className={styles.heading}>250$ per session</p>
+          <ul>
+            <li>Bridal Glam</li>
+            <li>Editorial Glam</li>
+            <li>Henna Glam</li>
+            <li>Everyday Glam</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Bridal</h3>
+          <p className={styles.heading}>250$ per session</p>
+          <ul>
+            <li>Bridal Glam</li>
+            <li>Editorial Glam</li>
+            <li>Henna Glam</li>
+            <li>Everyday Glam</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Bridal</h3>
+          <p className={styles.heading}>250$ per session</p>
+          <ul>
+            <li>Bridal Glam</li>
+            <li>Editorial Glam</li>
+            <li>Henna Glam</li>
+            <li>Everyday Glam</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
