e31b1f99dd644b062825b1e2f5822bd3120de9f8
Eman <emmanuelgatwech@gmail.com>
2020-02-07 06:15:07 +0300

    tiny modifications

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,25 +15,39 @@ import Gallery from "../components/gallery"
 const IndexPage = () => (
   <Layout>
     <SEO Claudia Acham="Home" />
-    <div className='wrapper'>
-      <div className='content'>
-        <div className='bio'>
+    <div className="wrapper">
+      <div className="content">
+        <div className="bio">
           <div>
-            <h1 style={{ color: 'pink' }}>Beauty, Skincare  & more...</h1>
+            <h1 style={{ color: "pink" }}>Beauty, Skincare & more...</h1>
             <p>
-              Salaam! Claudia Acham is a self taught MUA, mainly based in Juba, South Sudan & Nairobi, Kenya. </p>
-              <p>We’ve been  dealing in the makeup industry since 2017,  offering makeup services ranging from bridal, portraits and photo shoots, fashion shows, prom nights, commercial advertisements and more.
+              Salaam! Claudia Acham is a self taught MUA, mainly based in Juba,
+              South Sudan & Nairobi, Kenya.{" "}
             </p>
-            <button className='action'>Book your Experience</button>
+            <p>
+              We’ve been dealing in the makeup industry since 2017, offering
+              makeup services ranging from bridal, portraits and photo shoots,
+              fashion shows, prom nights, commercial advertisements and more.
+            </p>
+            <button className="action">Book your Experience</button>
           </div>
         </div>
-        <div className='profile-image'>
-          <img src='https://images.unsplash.com/photo-1523824921871-d6f1a15151f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='' style={{ width: '100%', height: 'auto', transform: 'rotate(2deg)', margin: 8 }}/>
+        <div className="profile-image">
+          <img
+            src="https://images.unsplash.com/photo-1528791067602-aee628b626fb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            alt=""
+            style={{
+              width: "100%",
+              height: "auto",
+              transform: "rotate(2deg)",
+              margin: 8,
+            }}
+          />
         </div>
       </div>
     </div>
     <Hero />
-    <div className='body'>
+    <div className="body">
       <div>
         <Products />
         <Gallery />
