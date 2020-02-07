594577c2e918d1a7fe6470a5b6fbfb2a5515a74f
Eman <emmanuelgatwech@gmail.com>
2020-02-07 16:25:49 +0300

    added analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,13 @@ module.exports = {
         icon: `src/images/favicon-16x16.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-157985888-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
